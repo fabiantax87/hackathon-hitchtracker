46b36bd09d17c48695c577665f303cfd2716d944
luuk0406 <luukf0406@gmail.com>
2023-10-04 14:22:51 +0200

    add footer to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Navigation from '@/components/navigation/Navigation'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Footer from '@/components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
       <Navigation />
-        {children}</body>
+        {children}
+      <Footer />
+        </body>
     </html>
   )
 }
